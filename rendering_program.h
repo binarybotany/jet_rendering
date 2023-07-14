6caf852971b4493567c5c55ea493299d579d139d
Katja <xatja@protonmail.com>
2023-07-14 14:56:29 +0200

    Initial commit

---
+++ rendering_program.h
@@ -0,0 +1,8 @@
+#ifndef JET_RENDERING_RENDERING_PROGRAM_H_
+#define JET_RENDERING_RENDERING_PROGRAM_H_
+
+namespace JET::rendering {
+class RenderingProgam {};
+}  // namespace JET::rendering
+
+#endif
